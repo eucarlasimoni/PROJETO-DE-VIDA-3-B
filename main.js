7279f413ca2ec297c39f970549488eda87a42c99
eucarlasimoni <161639492+eucarlasimoni@users.noreply.github.com>
2025-05-06 16:23:35 -0300

    06-05

---
+++ main.js
@@ -0,0 +1,13 @@
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes);
+
+for(let i=0;i <botoes.length; i++){
+
+    for(let j=0;j<botoes.length;j++){
+        botoes[j].classList.remove("ativo");
+    }
+
+    botoes[i].onclickm = function (){
+        botoes[i].classList.add("ativo");
+    }
+}
